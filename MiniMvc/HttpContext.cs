c7c7f761968af1412a37120027934bd954c2414e
wangyubo <1104725483@qq.com>
2022-10-30 21:38:33 +0800

    处理静态html、js、css资源

---
+++ MiniMvc/HttpContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MiniMvc
+{
+    /// <summary>
+    /// http请求上下文
+    /// </summary>
+    public class HttpContext
+    {
+        /// <summary>
+        /// 原始http请求数据
+        /// </summary>
+        public byte[] OriginalRequestDatas { get; set; }
+
+        /// <summary>
+        /// tcp连接
+        /// </summary>
+        public TcpClient TcpClient { get; set; }
+
+        /// <summary>
+        /// http请求
+        /// </summary>
+        public HttpRequest Request { get; set; }
+
+        /// <summary>
+        /// http响应
+        /// </summary>
+        public HttpResponse Response { get; set; }
+    }
+}
---
+++ MiniMvc/HttpResponse.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MiniMvc
+{
+    public class HttpResponse
+    {
+        private NetworkStream networkStream;
+        public HttpResponse(NetworkStream networkStream)
+        {
+            this.networkStream = networkStream;
+        }
+
+        /// <summary>
+        /// http版本
+        /// </summary>
+        public string HttpVersion { get; set; }
+
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public int StatusCode { get; set; }
+
+        /// <summary>
+        /// 状态文本
+        /// </summary>
+        public string StatusMessage { get; set; }
+
+        /// <summary>
+        /// 响应头
+        /// </summary>
+        public Dictionary<string, string> Headers { get; set; }
+
+        /// <summary>
+        /// 响应体
+        /// </summary>
+        public byte[] Body { get; set; }
+
+        /// <summary>
+        /// 写入响应流
+        /// </summary>
+        public void Write()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"{HttpVersion} {StatusCode} {StatusMessage}\r\n");
+            if (Headers != null)
+            {
+                foreach (var key in Headers.Keys)
+                {
+                    stringBuilder.Append($"{key}:{Headers[key]}\r\n");
+                }
+            }
+            if (Body == null)
+            {
+                stringBuilder.Append($"Content-Length:0\r\n");
+            }
+            else
+            {
+                stringBuilder.Append($"Content-Length:{Body.Length}\r\n");
+            }
+            stringBuilder.Append("\r\n");
+            networkStream.Write(Encoding.UTF8.GetBytes(stringBuilder.ToString()));
+            networkStream.Write(Body);
+        }
+    }
+}
